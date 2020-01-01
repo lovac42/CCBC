f11bb59b2943ec37e66c64d3655173064fb8c441
lovac42 <lovac42@protonmail.com>
2020-01-01 03:18:29 -0500

    Updated copyright info to include 2020

--- aqt/pinnedmodules.py
+++ aqt/pinnedmodules.py
@@ -1,5 +1,9 @@
-# Copyright: Ankitects Pty Ltd and contributors
+# -*- coding: utf-8 -*-
+# Copyright 2019-2020 Lovac42
+# Copyright 2006-2019 Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+# Support: https://github.com/lovac42/CCBC
+
 
 # this file is imported as part of the bundling process to ensure certain
 # modules are included in the distribution
