8942370b0c368e17484023a9f5277fa3c8ca55fd
lovac42 <lovac42@protonmail.com>
2020-04-23 02:02:27 -0400

    Updated base import module from 2.1.24. SRC taken from april 20 2020 (1661cad)

--- anki/importing/base.py
+++ anki/importing/base.py
@@ -1,25 +1,33 @@
-# -*- coding: utf-8 -*-
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-from anki.utils import  maxID
+from typing import Any, List, Optional
+
+from anki.collection import _Collection
+from anki.utils import maxID
 
 # Base importer
 ##########################################################################
+
 
 class Importer:
 
     needMapper = False
     needDelimiter = False
+    dst: Optional[_Collection]
 
-    def __init__(self, col, file):
+    def __init__(self, col: _Collection, file: str) -> None:
         self.file = file
-        self.log = []
+        self.log: List[str] = []
+
+        # This requires py3.8 which I am not ready to upgrade.
+        # self.col = col.weakref()
         self.col = col
+
         self.total = 0
         self.dst = None
 
-    def run(self):
+    def run(self) -> None:
         pass
 
     # Timestamps
@@ -28,9 +36,9 @@ class Importer:
     # and a previous import may have created timestamps in the future, so we
     # need to make sure our starting point is safe.
 
-    def _prepareTS(self):
+    def _prepareTS(self) -> None:
         self._ts = maxID(self.dst.db)
 
-    def ts(self):
+    def ts(self) -> Any:
         self._ts += 1
         return self._ts
